4cacc443cdde75d3aa29947722b280c4c790ca13
Iqbal Fauzi <iqbalfawz@gmail.com>
2017-09-04 16:18:13 +0700

    apply table index

--- modules/user-phone/_db/install.sql
+++ modules/user-phone/_db/install.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `user_phone` (
     `id` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `user` INTEGER NOT NULL,
-    `number` VARCHAR(25) NOT NULL,
+    `number` VARCHAR(25) NOT NULL UNIQUE,
     -- 1 Unverified
     -- 2 Verified
     -- 3 Default
@@ -9,3 +9,4 @@ CREATE TABLE IF NOT EXISTS `user_phone` (
     `updated` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+CREATE INDEX `by_user` ON `user_phone` ( `user` );
